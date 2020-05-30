92f32900729aebb33bf69a2425f83db1eebec78b
Juan Camilo Arias <jcamiloat@gmail.com>
2020-05-30 14:44:38 -0500

    2.9 Phonebook - Number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
 
 import PersonDetails from './components/PersonDetails';
+import Input from './components/Input';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const addNewPerson = (event) => {
     event.preventDefault();
+    const isDuplicateEntry = persons.some((person) => person.name === newName);
+    if (isDuplicateEntry) {
+      alert(`"${newName}" already exists, please use another name.`);
+      return;
+    }
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(newPerson));
     setNewName('');
@@ -24,7 +41,16 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
         <div>
-          name: <input value={newName} onChange={handleNewNameChange} />
+          <Input
+            label="name"
+            inputValue={newName}
+            onChangeHandler={handleNewNameChange}
+          />
+          <Input
+            label="number"
+            inputValue={newNumber}
+            onChangeHandler={handleNewNumberChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
--- part2/phonebook/src/components/PersonDetails.js
+++ part2/phonebook/src/components/PersonDetails.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const PersonDetails = ({ person }) => <p>{person.name}</p>;
+const PersonDetails = ({ person }) => (
+  <p>{`${person.name} /// ${person.number}`}</p>
+);
 
 export default PersonDetails;

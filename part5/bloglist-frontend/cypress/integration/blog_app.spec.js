fb74b3790b6f32bd7fd4155dbc7c361963cbeb54
jariast <jcamiloat@gmail.com>
2020-06-23 15:20:16 -0500

    Fixing some npm issues. Adding Cypress intellisense.

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,9 +1,9 @@
 describe('Blog App', function () {
-  // beforeEach(function(){
-  //     cy
-  // })
-  it('loads', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
     cy.visit('http://localhost:3000/');
+  });
+  it('Shows Login Form', function () {
     cy.contains('Login into the app');
   });
 });

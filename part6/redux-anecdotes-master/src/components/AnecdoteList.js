7d47c4cd9b1148ce9337e01d203bda36449688c5
jariast <jcamiloat@gmail.com>
2020-07-08 11:16:08 -0500

    6.11 Notification state.

--- part6/redux-anecdotes-master/src/components/AnecdoteList.js
+++ part6/redux-anecdotes-master/src/components/AnecdoteList.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { vote } from '../reducers/anecdoteReducer';
+import { setMessage, clearMsg } from '../reducers/notificationReducer';
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) =>
     state.anecdotes.sort((a, b) => b.votes - a.votes)
   );
   const dispatch = useDispatch();
+
+  // TODO Compare this implementation with the solution after submitting the code
+  const handleVoteClick = (anecdote) => {
+    dispatch(vote(anecdote.id));
+    dispatch(setMessage(`You voted for "${anecdote.content}"`));
+    setTimeout(() => {
+      dispatch(clearMsg());
+    }, 5000);
+  };
 
   return (
     <>
@@ -16,7 +26,7 @@ const AnecdoteList = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+            <button onClick={() => handleVoteClick(anecdote)}>vote</button>
           </div>
         </div>
       ))}
--- part6/redux-anecdotes-master/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes-master/src/reducers/notificationReducer.js
@@ -1,12 +1,27 @@
-const notificationReducer = (state = 'Test msg', action) => {
+const notificationReducer = (state = null, action) => {
   console.log('state before action: ', state);
   console.log('action', action);
   switch (action.type) {
     case 'SET_MESSAGE':
       return action.msg;
+    case 'CLEAR_MESSAGE':
+      return null;
     default:
       return state;
   }
+};
+
+export const setMessage = (message) => {
+  return {
+    type: 'SET_MESSAGE',
+    msg: message,
+  };
+};
+
+export const clearMsg = () => {
+  return {
+    type: 'CLEAR_MESSAGE',
+  };
 };
 
 export default notificationReducer;
